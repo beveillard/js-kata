2674120a003d3b4707d5db215097933cdfd09408
Benoît Eveillard <benoit.eveillard@gmail.com>
2020-12-27 16:26:59 -0500

    Creating ATM

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,22 @@
-import { systemUnderTest } from ".";
+import { atm } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
-  });
+it("Should return 1 x 500 bill", function () {
+  // Given
+  // When
+  // Then
+  expect(atm(500)).toEqual([1, 0, 0, 0, 0, 0]);
+});
+
+it("Should return 1 x 10 bill", function () {
+  // Given
+  // When
+  // Then
+  expect(atm(10)).toEqual([0, 0, 0, 0, 0, 1]);
+});
+
+it("Should return 1 x 200 + 1 x 50 + 1 x 20 bills", function () {
+  // Given
+  // When
+  // Then
+  expect(atm(270)).toEqual([0, 1, 0, 1, 1, 0]);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
-export function systemUnderTest(): boolean {
-  return true;
+export function atm(amount) {
+  let withdraw500 = withdraw(amount, 500); amount = withdraw500[1];
+  let withdraw200 = withdraw(amount, 200); amount = withdraw200[1];
+  let withdraw100 = withdraw(amount, 100); amount = withdraw100[1];
+  let withdraw50 = withdraw(amount, 50); amount = withdraw50[1];
+  let withdraw20 = withdraw(amount, 20); amount = withdraw20[1];
+  let withdraw10 = withdraw(amount, 10); amount = withdraw10[1];
+
+  return [
+    withdraw500[0],
+    withdraw200[0],
+    withdraw100[0],
+    withdraw50[0],
+    withdraw20[0],
+    withdraw10[0]
+  ];
+}
+
+function withdraw(amount, billValue) {
+  let rest = amount % billValue;
+  let numberOfBills = (amount - rest) / billValue;
+
+  return [numberOfBills, rest];
 }
